a86baa3f26c1a0d0a84fbbea0bcec18d9d9c68eb
EdisAnthony <edisanthony@gmail.com>
2022-01-19 22:04:06 -0600

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'videoplayer-angular';
+  falseValue(e:Event):boolean{
+    e.preventDefault();
+    return false;
+  }
 }
